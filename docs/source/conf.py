a6daaa75190307c57c5d493a4f40594d66660654
Qian Luo <qianqianluo@gmail.com>
2021-10-26 11:51:46 -0700

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,6 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-language = None
-locale_dirs = ['locale/']   # path is example but recommended.
+
 gettext_compact = False 
+language = "zh_CN"
+locale_dirs = ["locale/"]
